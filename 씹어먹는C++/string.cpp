b901c4e070dde407738020cfac116cb60f2e19fc
Young Zoo Park <enkeejuniour@naver.com>
2022-06-20 11:29:30 +0900

    string class

--- 씹어먹는C++/string.cpp
+++ 씹어먹는C++/string.cpp
@@ -7,23 +7,33 @@ class MyString {
 	char* string_content;
 	// 문자열 데이터의 길이
 	int string_length;
+	// 현재 할당된 용량
+	int memory_capacity;
 public:
 	MyString(char c);
 	MyString(const char* str);
 	MyString(const MyString& str);
 	~MyString();
 	int length() const;
+	int capacity() const;
+	void reserve(int size);
 	void print();
 	MyString& assign(const MyString& str);
 	MyString& assign(const char* str);
+	char at(int i) const;
+	MyString& insert(int loc, const MyString& str);
+	MyString& insert(int loc, const char* str);
+	MyString& insert(int loc, char c);
 };
 MyString::MyString(char c) {
 	string_content = new char[1];
 	string_content[0] = c;
 	string_length = 1;
+	memory_capacity = 1;
 }
 MyString::MyString(const char* str) {
 	string_length = strlen(str);
+	memory_capacity = string_length;
 	string_content = new char[string_length + 1];
 	for (int i = 0; i < string_length; i++)
 	{
@@ -32,6 +42,7 @@ MyString::MyString(const char* str) {
 }
 MyString::MyString(const MyString& str) {
 	string_length = str.string_length;
+	memory_capacity = str.string_length;
 	string_content = new char[string_length + 1];
 	for (int i = 0; i < string_length; i++)
 	{
@@ -51,15 +62,59 @@ void MyString::print() {
 	}
 }
 MyString& MyString::assign(const MyString& str) {
-
+	if (str.string_length > memory_capacity) {
+		delete[] string_content;
+		string_content = new char[str.string_length];
+		memory_capacity = str.string_length;
+	}
+	for (int i = 0; i < str.string_length; i++)
+	{
+		string_content[i] = str.string_content[i];
+	}
+	string_length = str.string_length;
+	return *this;
 }
 MyString& MyString::assign(const char* str) {
-
+	int str_length = strlen(str);
+	if (str_length > memory_capacity) {
+		delete[] string_content;
+		string_content = new char[str_length];
+		memory_capacity = str_length;
+	}
+	for (int i = 0; i < str_length; i++)
+	{
+		string_content[i] = str[i];
+	}
+	string_length = str_length;
+	return *this;
 }
+int MyString::capacity() const { return memory_capacity; }
+void MyString::reserve(int size) {
+	if (size > memory_capacity) {
+		char* prev_string_content = string_content;
+		string_content = new char[size];
+		memory_capacity = size;
+		for (int i = 0; i < string_length; i++)
+		{
+			string_content[i] = prev_string_content[i];
+		}
+		delete[] prev_string_content;
+	}
+}
+char MyString::at(int i) const {
+	if (i >= string_length || i < 0) return NULL;
+	else {
+		return string_content[i];
+	}
+}
+MyString& MyString::insert(int loc, const MyString& str);
+MyString& MyString::insert(int loc, const char* str);
+MyString& MyString::insert(int loc, char c);
 int main() {
-	MyString str1("Hello world!");
-	MyString str2(str1);
-	str1.print();
-	str2.print();
+	MyString str1("very very long string");
+	str1.reserve(30);
+	std::cout << "capacity : " << str1.capacity() << std::endl;
+	std::cout << "length : " << str1.length() << std::endl;
+
 	return 0;
 }
