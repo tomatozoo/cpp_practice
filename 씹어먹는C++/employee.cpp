4ac5c5a7fd6e65f07895c1c88d323fa091c1bb6a
Young Zoo Park <enkeejuniour@naver.com>
2022-07-25 15:05:19 +0900

    드ㅔㅣㅐㄷㄷ

--- 씹어먹는C++/employee.cpp
+++ 씹어먹는C++/employee.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 #include <string>
 
+class Employee {
+	std::string name;
+	int age;
+
+	std::string position;
+	int rank;
+	
+public:
+	Employee(std::string name, int age, std::string position, int rank)
+		: name(name), age(age), position(position), rank(rank) {}
+
+	// 복사 생성자
+	Employee(const Employee& employee) {
+		name = employee.name;
+		age = employee.age;
+		position = employee.position;
+		rank = employee.rank;
+	}
+
+	Employee() {}
+
+	void print_info() {
+		std::cout<< name << " (" <<position << " , "<<age << ") ==> " << calculate_pay() << "만원" << std::endl;
+	}
+
+	int calculate_pay() { return 200 + rank * 50; }
+};
+
+class EmployeeList {
+	int alloc_employee; // 데이터의 크기
+	int current_employee; // 실제 양
+	Employee** employee_list;
+
+
+};
 int main() {
 	return 0;
 }
