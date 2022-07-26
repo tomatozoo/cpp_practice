a67eb237ced57caa88688604c543dab2fe5d222a
Young Zoo Park <enkeejuniour@naver.com>
2022-07-26 11:23:33 +0900

    employee

--- 씹어먹는C++/employee.cpp
+++ 씹어먹는C++/employee.cpp
@@ -34,7 +34,33 @@ class EmployeeList {
 	int current_employee; // 실제 양
 	Employee** employee_list;
 
+public:
+	EmployeeList(int alloc_employee) : alloc_employee(alloc_employee) {
+		employee_list = new Employee * [alloc_employee];
+		current_employee = 0;
+	}
 
+	void add_employee(Employee* employee) {
+		employee_list[current_employee] = employee;
+		current_employee++;
+	}
+
+	int current_employee_num() { return current_employee; }
+	void print_employee_info() {
+		int total_pay = 0;
+		for (int i = 0; i < current_employee; i++)
+		{
+			employee_list[i]->print_info();
+			total_pay += employee_list[i]->calculate_pay();
+		}
+		std::cout << "총 임금 : " << total_pay << std::endl;
+	}
+	~EmployeeList() {
+		for (int i = 0; i < current_employee; i++) {
+			delete employee_list[i];
+		}
+		delete[] employee_list;
+	}
 };
 int main() {
 	return 0;
