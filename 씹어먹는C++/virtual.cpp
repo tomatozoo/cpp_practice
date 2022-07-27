3b29e712c1dbf1d9e4fda7de5dd8aeec94876a52
Young Zoo Park <enkeejuniour@naver.com>
2022-07-27 11:19:32 +0900

    ~lab11

---
+++ 씹어먹는C++/virtual.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iostream>
+
+class Parent {
+public:
+	Parent() { std::cout << "Parent 생성자 호출" << std::endl; }
+	virtual ~Parent() { std::cout << "Parent 소멸자 호출" << std::endl; }
+};
+class Child : public Parent {
+public:
+	Child() : Parent() { std::cout << "Child 생성자 호출" << std::endl; }
+	~Child() { std::cout << "Child 소멸자 호출" << std::endl; }
+};
+
+int mn() {
+	std::cout << "평범한 Child 생성" << std::endl;
+	{
+		Child c;
+	}
+
+	std::cout << "Parent 포인터로 Child를 가리킬 때" << std::endl;
+	{
+		Parent* p = new Child();
+		delete p;
+		// 이때는 Child 소멸자가 호출되지 않아 memory leak 발생
+	}
+
+	{
+		Child ch;
+		Parent& par = ch;
+	}
+}
