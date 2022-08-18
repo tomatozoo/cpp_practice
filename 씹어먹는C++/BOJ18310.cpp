73a6550bccf8acc83fb1381060d1a40fb9c27d81
Young Zoo Park <enkeejuniour@naver.com>
2022-08-18 15:07:26 +0900

    .

--- 씹어먹는C++/BOJ18310.cpp
+++ 씹어먹는C++/BOJ18310.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main18310() {
+	int n;
+	std::cin >> n;
+	std::vector<int> v;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		std::cin >> tmp;
+		v.push_back(tmp);
+	}
+	std::sort(v.begin(), v.end());
+
+	if (v.size() % 2 == 0) {
+		std::cout << v[v.size() / 2 -1]<< std::endl;
+	}
+	else {
+		std::cout << v[(v.size() -1)/ 2] << std::endl;
+	}
 	return 0;
 }
