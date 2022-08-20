ca84890c1c5e6818272916801b228cb1c05ef413
Young Zoo Park <enkeejuniour@naver.com>
2022-08-20 10:01:05 +0900

    .

---
+++ 씹어먹는C++/DP.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int fib(int n) {
+	if (n > 1) {
+		int* fibo = new int[n];
+		fibo[0] = 0;
+		fibo[1] = 1;
+
+		for (int i = 2; i < n; i++) {
+			fibo[i] = fibo[i - 1] + fibo[i - 2];
+		}
+		return fibo[n - 1];
+	}
+}
+int main() {
+
+	std::cout << fib(2) << std::endl;
+	return 0;
+} 
